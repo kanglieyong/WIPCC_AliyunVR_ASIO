113effab97e47fd028d6afc4c6e1048d48cd6f76
kly <kanglieyong@gmail.com>
2020-05-28 22:17:00 +0800

    loop support

--- Main.cpp
+++ Main.cpp
@@ -18,10 +18,8 @@ using namespace std;
 
 string appKey/* = "oI8amHfNKhLvO87K"*/;
 string accessToken/* = "18de77cbf3444c2390465fe3f6de2c7e"*/;
-string vrfile{""};
-long content_len/* = 94694*/;
 
-void http_connect(iostream& s, int& sz);
+void http_connect(shared_ptr<iostream> s, const string& vrfile, size_t content_len, int& sz);
 
 int main(int argc, char *argv[])
 {
@@ -39,94 +37,103 @@ int main(int argc, char *argv[])
     }
   }
   //cout << "accessToken: " << accessToken << "\nappKey: " << appKey << endl;
+
+  string server("nls-gateway.cn-shanghai.aliyuncs.com");
+  auto s = make_shared<boost::asio::ip::tcp::iostream>(server, "http");
   
-  // 2. Read media file
-  if (argc == 2) {
-    vrfile = string(argv[1]);
-  } else {
-    vrfile = "nls-sample-16k.wav"; 
-  }
-  //boost::filesystem::path media_path = boost::filesystem::current_path() / vrfile;
-  //content_len = boost::filesystem::file_size(media_path);
-  fstream vrfstr(vrfile, ios::in | ios::binary);
-  long start = vrfstr.tellg();
-  vrfstr.seekg(0, ios::end);
-  long end = vrfstr.tellg();
-  vrfstr.close();
-  content_len = end - start;
-  //cout << to_string(content_len) << endl;
-  //return 0;
+  int counter{0};
+  for (;;) {
+    std::cout << "[ " << counter++ << " ] WavName: ";
+    string vrfile;
+    getline(cin, vrfile);
+    if (vrfile.empty()) {
+      cout << "empty, QUIT!\n";
+      break;
+    }
+    // 2. Read media file
+    //boost::filesystem::path media_path = boost::filesystem::current_path() / vrfile;
+    //content_len = boost::filesystem::file_size(media_path);
+    fstream vrfstrm(vrfile, ios::in | ios::binary);
+    size_t content_len = 0;
+    long start = vrfstrm.tellg();
+    vrfstrm.seekg(0, ios::end);
+    long end = vrfstrm.tellg();
+    vrfstrm.close();
+    content_len = end - start;
+    //cout << to_string(content_len) << endl;
+    //return 0;
 
-  // 3. Connect to Server
-  try {
-    string server = "nls-gateway.cn-shanghai.aliyuncs.com";
-    boost::asio::ip::tcp::iostream s{server, "http"};
-    int body_len = 0;
-    http_connect(s, body_len);
+    // 3. Connect to Server
+    try {
+      int body_len = 0;
+      http_connect(s, vrfile, content_len, body_len);
 
-    auto body = std::make_unique<char[]>(body_len);
-    s.read(body.get(), body_len);
-    s.close();
+      // Http Response: Parse Body
+      auto body = std::make_unique<char[]>(body_len);
+      s->read(body.get(), body_len);
 
-    string resp_str;
-    std::copy(body.get(), body.get() + body_len, back_inserter(resp_str));
-    //cout << resp_str << endl;
-    stringstream ss;
-    ss << resp_str;
+      string resp_str;
+      std::copy(body.get(), body.get() + body_len, back_inserter(resp_str));
+      //cout << resp_str << endl;
+      stringstream ss;
+      ss << resp_str;
 
-    boost::property_tree::ptree tree;
-    boost::property_tree::read_json(ss, tree);
-    string task_id = tree.get<string>("task_id");
-    string result  = tree.get<string>("result");
-    int    status  = tree.get<int>("status");
-    string message = tree.get<string>("message");
+      boost::property_tree::ptree tree;
+      boost::property_tree::read_json(ss, tree);
+      string task_id = tree.get<string>("task_id");
+      string result  = tree.get<string>("result");
+      int    status  = tree.get<int>("status");
+      string message = tree.get<string>("message");
 
 #ifdef _WIN32
-    result = boost::locale::conv::between(result, "GBK", "UTF-8");
+      result = boost::locale::conv::between(result, "GBK", "UTF-8");
 #endif
-    cout << "{\n\t\"task_id\":\"" << task_id 
+      cout << "Response:\n{\n\t\"task_id\":\"" << task_id 
         << "\", \n\t\"result\": \"" << result 
         << "\", \n\t\"status\": " << status 
         << ",\n\t\"message\": \"" << message
-        << "\"\n}" << endl;
-  } catch (std::exception &e) {
-    cout << "Exception:" << e.what() << "\n";
-    return 1;
+        << "\"\n}" << endl << endl;
+    } catch (std::exception &e) {
+      cout << "Exception:" << e.what() << "\n";
+      return 1;
+    }
   }
 
   return 0;
 }
 
-void http_connect(iostream& s, int& len)
+void http_connect(shared_ptr<iostream> s, const string& vrfile, size_t content_len, int& body_len)
 {
   if (!s)
     throw runtime_error("can't connect\n");
 
-  s << "POST " << "/stream/v1/asr?appkey=" + appKey + "&sample_rate=8000" + " HTTP/1.1\r\n";
-  s << "HOST:" << "nls-gateway.cn-shanghai.aliyuncs.com\r\n";
-  s << "Content-Length:" << to_string(content_len) << "\r\n";
-  s << "Content-type:application/octet-stream\r\n";
-  s << "X-NLS-Token:" << accessToken << "\r\n";
-  s << "Accept-Encoding: gzip" << "\r\n";
-  s << "\r\n";
+  // Http Request: SetHeader
+  *s << "POST " << "/stream/v1/asr?appkey=" + appKey + "&sample_rate=8000" + " HTTP/1.1\r\n";
+  *s << "HOST:" << "nls-gateway.cn-shanghai.aliyuncs.com\r\n";
+  *s << "Content-Length:" << to_string(content_len) << "\r\n";
+  *s << "Content-type:application/octet-stream\r\n";
+  *s << "X-NLS-Token:" << accessToken << "\r\n";
+  *s << "Accept-Encoding: gzip" << "\r\n";
+  *s << "\r\n";
 
+  // Http Request: SetBody
   fstream ofstr(vrfile.c_str(), ios::in | ios::binary);
   //auto t0 = chrono::system_clock::now();
-  //s << ofstr.rdbuf();
   auto buff = std::make_unique<char[]>(content_len);
   ofstr.read(buff.get(), content_len);
-  s.write(buff.get(), content_len);
-  s.flush();
+  s->write(buff.get(), content_len);
+  s->flush();
   //auto t1 = chrono::system_clock::now();
   ofstr.close();
 
+  // Http Response: Parse Header
   string http_version;
   unsigned int status_code;
   //auto t2 = chrono::system_clock::now();
-  s >> http_version >> status_code;
+  *s >> http_version >> status_code;
   //auto t3 = chrono::system_clock::now();
   string status_message;
-  getline(s, status_message);
+  getline(*s, status_message);
 
   //cout << "request  costs: " << chrono::duration_cast<chrono::milliseconds>(t1 - t0).count() << " milliseconds\n"
 	  // << "response costs: " << chrono::duration_cast<chrono::milliseconds>(t3 - t2).count() << " milliseconds\n";
@@ -138,10 +145,10 @@ void http_connect(iostream& s, int& len)
     throw runtime_error{"Response returned with status code"};
 
   string header;
-  while (getline(s, header) && header !="\r") {
+  while (getline(*s, header) && header !="\r") {
     if ( header.substr(0, 14) == "Content-Length" ) {
       string::size_type end_pos = header.find('\r');
-      len = stoi(header.substr(16, end_pos-16)); 
+      body_len = stoi(header.substr(16, end_pos-16)); 
       //std::cout << "len:" << len << endl;
     }
   }
--- README.md
+++ README.md
@@ -6,6 +6,19 @@
  $ make run #run
 ```
 
+On Linux Platform, we can command `arecord` to generate sample file.
+
+##### Check Microphone
+```
+  $ arecord -l
+```
+##### Record
+* the `d` flags means how many seconds to record, for the following sample, we record `10s`.
+```
+  $ arecord -d 10 /tmp/test.wav
+```
+
+
 #### Windows
 * Currently only `VS2015 Win32 Debug` mode supported, but it's easy to support `VS2013/15/17/19 Win32/Win64 Debug/Release` since the `boost.asio` api changes little.
 
